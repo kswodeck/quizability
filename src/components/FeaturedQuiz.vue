a3c013ae0a04a76de4e0314f7147b14c5188b4a0
Kristoffer Swodeck <kswodeck@yahoo.com>
2022-11-10 18:22:39 -0800

    update featured

--- src/components/FeaturedQuiz.vue
+++ src/components/FeaturedQuiz.vue
@@ -1,17 +1,22 @@
 <template>
   <div v-if="quizData" id="featured-quiz-container">
-    <button class="featured-quiz-button">
-      <h2 class="quiz-heading">{{ quizData.title }}</h2>
-<span class="quiz-category">{{ quizData.category}}</span>
-    </button>
+    
+    <ion-button :router-link="`/tabs/take/quiz/${quizData.id}`" class="featured-quiz-button">
+      <div class="featured-quiz-div">
+        <h2 class="quiz-heading">{{ quizData.title }}</h2><br>
+      <span class="quiz-category">{{ quizData.category}}</span>
+      </div>
+    </ion-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { IonButton } from '@ionic/vue';
 
 export default defineComponent({
   name: 'featured-quiz',
+  components: { IonButton},
   props: {
     quizData: {
       type: Object,
@@ -34,6 +39,16 @@ methods: {
       width: 100%;
       height: calc(100px + 15vh);
       max-height: 20vh;
+      white-space: pre-wrap;
+      .featured-quiz-div {
+        .quiz-heading {
+          font-size: calc(1rem + 0.6vw);
+
+        }
+        .quiz-category {
+          font-size: calc(0.7rem + 0.4vw);
+        }
+      }
     }
 }
 
